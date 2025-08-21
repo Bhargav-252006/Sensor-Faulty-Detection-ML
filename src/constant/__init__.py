359e78902902bb672297bfe62331e74db3aa42b6
Bhargav-252006 <sakilambhargav@gmail.com>
2025-08-21 19:52:14 +0530

    final update

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,13 +1,19 @@
 import os
 
 
-AWS_S3_BUCKET_NAME = "wafer-fault"
+AWS_S3_BUCKET_NAME = "sensor-faulty"
+AWS_ACCESS_KEY="AKIA3GRS27XW77DAPWU5"
+AWS_SECRET_ACCESS_KEY="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"
+AWS_DEFAULT_REGION="US East (N. Virginia)"
+AWS_ECR_REPO_URI="769979514349.dkr.ecr.us-east-1.amazonaws.com/sensor-final-deployment"
+
+
 MONGO_DATABASE_NAME = "ML"
 MONGO_COLLECTION_NAME = "Sensor"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://sakilambhargav:bhargav2006@mlprojects.gnjntpy.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URL="mongodb+srv://sakilambhargav:bhargav2006@mlprojects/ML"#url//username  pass @cluster/db name
 
 
 MODEL_FILE_NAME = "model"
